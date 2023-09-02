f7fa3279fdb2c936829fc164a395fa523e437f94
nissan95sx <69825621+nissan95sx@users.noreply.github.com>
2023-09-02 09:10:46 -0500

    Create Submission.js

---
+++ Submission.js
@@ -0,0 +1,26 @@
+const findSum = ([2, 4, 6]);
+
+const sum = findSum.reduce(function(sum, number){
+    return sum + number;
+}, 0);
+};
+
+const findFrequency = function(array) {
+  // your code here - don't forget to return an object!
+};
+
+const isPalindrome = function(str) {
+  // your code here - don't forget to return a boolean!
+};
+
+const largestPair = function(array) {
+  // your code here - don't forget to return a number!
+};
+
+const removeParenth = function(str) {
+  // your code here - don't forget to return a string!
+};
+
+const scoreScrabble = function(str) {
+  // your code here - don't forget to return a number!
+};
