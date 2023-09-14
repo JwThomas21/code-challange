f74e76b03b30ebec4491ed4f0bda0903aff69288
Justin Thomas <69825621+nissan95sx@users.noreply.github.com>
2023-09-14 18:19:27 -0500

    Update Submission.js

--- Submission.js
+++ Submission.js
@@ -1,8 +1,8 @@
 const findSum = function(array) {
-    if (numbers.length === 0) {
+    if (array.length === 0) {
         return 0;
     }
-    return numbers.reduce((sum, num) => sum = num, 0);
+    return array.reduce((sum, num) => sum = num, 0);
 }
 
 
