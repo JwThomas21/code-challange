772c50376a92ccef4d5426aa69cf0cf7ec3eacd3
Justin Thomas <69825621+nissan95sx@users.noreply.github.com>
2023-09-16 17:57:36 -0500

    Update Submission.js

--- Submission.js
+++ Submission.js
@@ -27,13 +27,7 @@ const findSum = function(array) {
           }
 
                 
-    
-
-
-            
-
-
-function isPalindrome(str) {
+    function isPalindrome(str) {
     var str = str.toLowerCase();
     var reversed = str.split("").reverse().join("");
     if (reversed === str) return true;
