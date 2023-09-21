4d24e376d07c5a1431b59f790103cfe8c908c8ae
Justin Thomas <69825621+JwThomas21@users.noreply.github.com>
2023-09-20 20:41:42 -0500

    Update Submission.js

--- Submission.js
+++ Submission.js
@@ -1,9 +1,10 @@
 const findSum = function(array) {
-    if (array.length === 0) {
-        return 0;
-    }
-    return array.reduce((sum, num) => sum = num, 0);
-};
+    let sum = 0;
+    for (let i = 0; i < array.length; i++){
+    sum += array[i];
+}
+return sum;
+}
 
 const findFrequency = function (array) {
 const map = {};
